68e26914c13625f81b633e586e89a09996e20550
MM-WebDeveloper <milan.varnamo@gmail.com>
2022-02-06 19:46:04 +0100

    added the login page and graphql gen login resolver

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLoginMutation } from '../generated/graphql';
 
 interface Props {}
 
-export const Login: React.FC<Props> = () => {
-	return <div>Login Page</div>;
+export const Login: React.FC = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [login] = useLoginMutation();
+	const navigate = useNavigate();
+
+	return (
+		<div>
+			<form
+				onSubmit={async (e) => {
+					e.preventDefault();
+					const res = await login({
+						variables: {
+							email,
+							password,
+						},
+					});
+
+					console.log(res.data?.login.accessToken);
+				}}
+			>
+				<div>
+					<input
+						value={email}
+						placeholder='email'
+						onChange={(e) => {
+							setEmail(e.target.value);
+						}}
+					/>
+				</div>
+				<div>
+					<input
+						value={password}
+						placeholder='password'
+						type='password'
+						onChange={(e) => {
+							setPassword(e.target.value);
+						}}
+					/>
+				</div>
+				<button type='submit'>login</button>
+			</form>
+		</div>
+	);
 };
